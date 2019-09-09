d933d4720b7ebfec99da209d7ba301ea35cd9020
Josiah Peterson <josiah.peterson@aol.com>
2019-09-09 03:41:09 -0700

    Implementing search mechanisms

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from flask import render_template
 from flask import request
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from whoosh.index import create_in
+from whoosh import highlight
+from whoosh.fields import *
+import re
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/canons.db'
+db = SQLAlchemy(app)
+
+class Canon(db.Model):
+        canon_num = db.Column(db.Integer, primary_key=True)
+        content = db.Column(db.Text)
+        section_id = db.Column(db.Integer)
+
+        def __repr__(self):
+                return '{0} {1}'.format(self.canon_num, self.content)
+
+class SectionTitle(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.Text)
+
+        def __repr__(self):
+                return '{0}'.format(self.title)
+
+db.create_all()
+
+schema = Schema(content=TEXT(stored=True), canon_num=STORED, section_id=STORED)
+ix = create_in("indexdir", schema)
+writer = ix.writer()
+for canon in Canon.query.all():
+        writer.add_document(content=str(canon.content), canon_num=canon.canon_num, section_id=canon.section_id)
+writer.commit()
 
 @app.route('/')
 def index():
@@ -10,11 +42,25 @@ def index():
 @app.route('/search', methods=['GET'])
 def search():
     if request.method == "GET":
-        return render_template('search.html', search_text=request.args["query"])
+        from whoosh.qparser import QueryParser
+        with ix.searcher() as searcher:
+                query = QueryParser("content", ix.schema).parse(request.args["query"])
+                results = searcher.search(query, limit=20, terms=True)
+                results.formatter = highlight.HtmlFormatter(tagname="span", classname="search-text")
+                query_result = []
+                for section_header in SectionTitle.query.all():
+                        section = {"title": section_header.title, "results": []}
+                        for result in results:
+                                if result["section_id"] == section_header.id:
+                                        section["results"].append(result)
+                        if len(section["results"]) > 0:
+                                query_result.append(section)
+                return render_template('search.html', search_text=request.args["query"], sections=query_result)
 
 @app.route('/canon', methods=['GET'])
 def canon():
-    return render_template('canon.html', search_text=request.args["query"], canon=request.args["canon"])
+    canon = Canon.query.filter_by(canon_num=int(request.args["canon"])).first()
+    return render_template('canon.html', search_text=request.args["query"], canon=canon)
 
 if __name__ == '__main__':
     app.run()
