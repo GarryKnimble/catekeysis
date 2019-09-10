5baad7f365b364e50621434724dee90d787756cc
Josiah Peterson <josiah.peterson@aol.com>
2019-09-09 18:43:07 -0700

    Change meta tags, changed titles

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ from whoosh.fields import *
 import os
 import re
 
+# Setup web app along with Sqlite database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/canons.db'
 db = SQLAlchemy(app)
 
+# Create tables for use for canons
+
+# Table: Canon
+# Description: Used to store all the canons in the CCC
+# Parameters:
+#       canon_num  - The number of the canon according to the CCC
+#       content    - The text of the canon from the CCC
+#       section_id - The section_id of the section that the canon
+#                    belongs to in the CCC
 class Canon(db.Model):
         canon_num = db.Column(db.Integer, primary_key=True)
         content = db.Column(db.Text)
@@ -21,6 +31,11 @@ class Canon(db.Model):
         def __repr__(self):
                 return '{0} {1}'.format(self.canon_num, self.content)
 
+# Table: SectionTitle
+# Description: Used to store all the section headers in the CCC
+# Parameters:
+#       id    - The assigned id for a particular section title
+#       title - The title of the section in the CCC
 class SectionTitle(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         title = db.Column(db.Text)
@@ -30,29 +45,37 @@ class SectionTitle(db.Model):
 
 db.create_all()
 
+# Set up the indexing tools
 schema = Schema(content=TEXT(stored=True), canon_num=STORED, section_id=STORED)
+# If there is no indexing files found, create and write to them
 if len(os.listdir('./indexdir')) == 0:
         ix = create_in("indexdir", schema)
         writer = ix.writer()
         for canon in Canon.query.all():
                 writer.add_document(content=str(canon.content), canon_num=canon.canon_num, section_id=canon.section_id)
         writer.commit()
+# Otherwise, open the currently existing indexing files
 else:
         ix = open_dir("indexdir")
 
+# Home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Search page
 @app.route('/search', methods=['GET'])
 def search():
     if request.method == "GET":
         from whoosh.qparser import QueryParser
+        # With the Whoosh search object...
         with ix.searcher() as searcher:
+                # Create query parser to search for canons from GET request arguments
                 query = QueryParser("content", ix.schema).parse(request.args["query"])
                 results = searcher.search(query, limit=20, terms=True)
-                results.formatter = highlight.HtmlFormatter(tagname="span", classname="search-text")
+                results.formatter = highlight.UppercaseFormatter()
                 query_result = []
+                # Go through section headers and separate canons according to proper sections
                 for section_header in SectionTitle.query.all():
                         section = {"title": section_header.title, "results": []}
                         for result in results:
@@ -60,12 +83,14 @@ def search():
                                         section["results"].append(result)
                         if len(section["results"]) > 0:
                                 query_result.append(section)
-                return render_template('search.html', search_text=request.args["query"], sections=query_result)
+                return render_template('search.html', search_text=request.args["query"], highlight=query_result[0]["results"][0].highlights("content"), sections=query_result)
 
+# Canon page
 @app.route('/canon', methods=['GET'])
 def canon():
-    canon = Canon.query.filter_by(canon_num=int(request.args["canon"])).first()
-    return render_template('canon.html', search_text=request.args["query"], canon=canon)
+    if request.method == "GET":
+        canon = Canon.query.filter_by(canon_num=int(request.args["canon"])).first()
+        return render_template('canon.html', search_text=request.args["query"], canon=canon)
 
 if __name__ == '__main__':
     app.run()
