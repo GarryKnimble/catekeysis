9b27f09e3ff399c57c46de3c3e7e13e2ba304fb4
JohnPhilosopher <josiah.peterson@aol.com>
2019-11-11 23:51:54 -0800

    Fixed wrong terminology

--- app.py
+++ app.py
@@ -14,25 +14,25 @@ import re
 
 # Setup web app along with Sqlite database
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/canons.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/paragraphs.db'
 db = SQLAlchemy(app)
 
-# Create tables for use for canons
+# Create tables for use for paragraphs
 
-# Table: Canon
-# Description: Used to store all the canons in the CCC
+# Table: Paragraph
+# Description: Used to store all the paragraphs in the CCC
 # Parameters:
-#       canon_num  - The number of the canon according to the CCC
-#       content    - The text of the canon from the CCC
-#       section_id - The section_id of the section that the canon
+#       paragraph_num  - The number of the paragraph according to the CCC
+#       content    - The text of the paragraph from the CCC
+#       section_id - The section_id of the section that the paragraph
 #                    belongs to in the CCC
-class Canon(db.Model):
-        canon_num = db.Column(db.Integer, primary_key=True)
+class Paragraph(db.Model):
+        paragraph_num = db.Column(db.Integer, primary_key=True)
         content = db.Column(db.Text)
         section_id = db.Column(db.Integer)
 
         def __repr__(self):
-                return '{0} {1}'.format(self.canon_num, self.content)
+                return '{0} {1}'.format(self.paragraph_num, self.content)
 
 # Table: SectionTitle
 # Description: Used to store all the section headers in the CCC
@@ -48,7 +48,7 @@ class SectionTitle(db.Model):
 
 class FootNote(db.Model):
         footnote = db.Column(db.Text, primary_key=True)
-        canon_num = db.Column(db.Integer)
+        paragraph_num = db.Column(db.Integer)
         sentence_location = db.Column(db.Integer)
         id = db.Column(db.Integer)
 
@@ -58,13 +58,13 @@ class FootNote(db.Model):
 db.create_all()
 
 # Set up the indexing tools
-schema = Schema(content=TEXT(stored=True), canon_num=STORED, section_id=STORED)
+schema = Schema(content=TEXT(stored=True), paragraph_num=STORED, section_id=STORED)
 # If there is no indexing files found, create and write to them
 if len(os.listdir('./indexdir')) == 0:
         ix = create_in("indexdir", schema)
         writer = ix.writer()
-        for canon in Canon.query.all():
-                writer.add_document(content=str(canon.content), canon_num=canon.canon_num, section_id=canon.section_id)
+        for paragraph in Paragraph.query.all():
+                writer.add_document(content=str(paragraph.content), paragraph_num=paragraph.paragraph_num, section_id=paragraph.section_id)
         writer.commit()
 # Otherwise, open the currently existing indexing files
 else:
@@ -101,12 +101,12 @@ def search():
         from whoosh.qparser import QueryParser
         # With the Whoosh search object...
         with ix.searcher() as searcher:
-                # Create query parser to search for canons from GET request arguments
+                # Create query parser to search for paragraphs from GET request arguments
                 query = QueryParser("content", ix.schema).parse(request.args["query"] if len(request.args["query"]) > 0 else "*")
-                results = searcher.search(query, limit=20, terms=True)
+                results = searcher.search(query, limit=None, terms=True)
                 results.formatter = highlight.UppercaseFormatter()
                 query_result = []
-                # Go through section headers and separate canons according to proper sections
+                # Go through section headers and separate paragraphs according to proper sections
                 for section_header in SectionTitle.query.all():
                         section = {"title": section_header.title, "results": []}
                         for result in results:
@@ -114,16 +114,17 @@ def search():
                                         section["results"].append(result)
                         if len(section["results"]) > 0:
                                 query_result.append(section)
-                return check_mode(render_template('search.html', search_text=request.args["query"], highlight_canon_num=(query_result[0]["results"][0]["canon_num"] if len(query_result) > 0 else ""), highlight=(query_result[0]["results"][0].highlights("content") if len(query_result) else "No Result"), has_results=(len(query_result) > 0), sections=query_result))
+                return check_mode(render_template('search.html', search_text=request.args["query"], highlight_paragraph_num=(query_result[0]["results"][0]["paragraph_num"] if len(query_result) > 0 else ""), highlight=(query_result[0]["results"][0].highlights("content") if len(query_result) else "No Result"), has_results=(len(query_result) > 0), sections=query_result))
 
-# Canon page
-@app.route('/canon', methods=['GET'])
-def canon():
+# Paragraph page
+@app.route('/paragraph', methods=['GET'])
+def paragraph():
     if request.method == "GET":
-        footnotes = FootNote.query.filter_by(canon_num=int(request.args["canon"]))
-        canon = Canon.query.filter_by(canon_num=int(request.args["canon"])).first()
-        canon.content = insert_footnotes(footnotes, "<sup><a class='footnote-item' href='#'>", "</a></sup>", canon.content)
-        return check_mode(render_template('canon.html', search_text=request.args["query"], canon=canon, footnotes=footnotes, footnotes_length=footnotes.count()))
+        paragraph = int(request.args["id"])
+        footnotes = FootNote.query.filter_by(paragraph_num=paragraph)
+        paragraph = Paragraph.query.filter_by(paragraph_num=paragraph).first()
+        paragraph.content = insert_footnotes(footnotes, "<sup><a class='footnote-item' href='#'>", "</a></sup>", paragraph.content)
+        return check_mode(render_template('paragraph.html', search_text=request.args["query"], paragraph=paragraph, footnotes=footnotes, footnotes_length=footnotes.count()))
 
 # Maintenance page
 @app.route('/maintenance', methods=['GET'])
---
+++ static/js/catekeysis.scrollreveal.js
@@ -0,0 +1,3 @@
+window.sr = ScrollReveal({ reset: false });
+
+sr.reveal('.church-father', { duration: 2000 }, 50);
