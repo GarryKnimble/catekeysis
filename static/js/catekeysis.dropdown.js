fcff8cfb2a45c621ba3e632d2b2b07e68e1fb952
JohnPhilosopher <josiah.peterson@aol.com>
2019-11-11 23:54:54 -0800

    Fixed javascript

--- static/js/catekeysis.dropdown.js
+++ static/js/catekeysis.dropdown.js
@@ -1,10 +1,10 @@
-$(".canon-list>li").click(function(){
+$(".paragraph-list>li").click(function(){
     if($(this).hasClass("collapsed")) {
-        $(".canon-list>li").addClass("collapsed");
+        $(".paragraph-list>li").addClass("collapsed");
         $(this).removeClass("collapsed");
     }
 });
-$(".canon-list>li>.wrapper>.collapse-btn").click(function(e){
+$(".paragraph-list>li>.wrapper>.collapse-btn").click(function(e){
     if(!$(this).parent().parent().hasClass("collapsed")) {
         $(this).parent().parent().addClass("collapsed");
         e.stopPropagation();
